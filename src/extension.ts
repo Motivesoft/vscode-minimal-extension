00045fbad14780059b2e03c55cfaeed02f0afec1
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2025-02-13 11:23:18 +0100

    Update to latest versions and add .gitignore

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,12 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
   if (vscode.env.uiKind === vscode.UIKind.Web) {
+    console.log("Extension is running in the web");
+
     vscode.window.showInformationMessage("Extension is running in the web");
   } else {
+    console.log("Extension is running on the desktop");
+    
     vscode.window.showInformationMessage("Extension is running on the desktop");
   }
 }
